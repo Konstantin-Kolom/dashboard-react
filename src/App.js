056fc80406e8e004f4c9478cb9a9abe0166c2ee2
Konstantin Kolomiec <77701872+Konstantin-Kolom@users.noreply.github.com>
2023-03-02 14:05:45 +0200

    fix1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import { ColorModeContext, useMode } from "./theem";
 import { CssBaseline, ThemeProvider } from "@mui/material";
+import { Route, Routes } from "react-router-dom";
+import { ProSidebarProvider } from "react-pro-sidebar";
 
 import Topbar from "./scenes/global/Topbar";
+import SidebarMenu from "./scenes/global/Sidebar";
+
+import { Dashboard } from "./scenes/dashboard/index.jsx";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -10,9 +15,26 @@ function App() {
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
+
         <div className="app">
+          <ProSidebarProvider>
+            <SidebarMenu />
+          </ProSidebarProvider>
           <main className="content">
             <Topbar />
+            <Routes>
+              {/* <Route path="/" element={<Dashboard />}></Route> */}
+              {/* <Route path="/team" element={<Dashboard />}></Route> */}
+              {/* <Route path="/contacts" element={<Dashboard />}></Route> */}
+              {/* <Route path="/invoices" element={<Dashboard />}></Route> */}
+              {/* <Route path="/form" element={<Dashboard />}></Route> */}
+              {/* <Route path="/calendar" element={<Dashboard />}></Route> */}
+              {/* <Route path="/faq" element={<Dashboard />}></Route> */}
+              {/* <Route path="/pie" element={<Dashboard />}></Route> */}
+              {/* <Route path="/bar" element={<Dashboard />}></Route> */}
+              {/* <Route path="/line" element={<Dashboard />}></Route> */}
+              {/* <Route path="/geography" element={<Dashboard />}></Route> */}
+            </Routes>
           </main>
         </div>
       </ThemeProvider>
